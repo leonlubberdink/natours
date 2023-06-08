8fdc7fcda01e00eec089f658af53845b98157106
leonlubberdink <mail@leonlubberdink.com>
2023-06-08 17:02:05 +0200

    Added comment in app.js

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ const viewRouter = require('./routes/viewRoutes');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 
+//Start express app
 const app = express();
 
 app.set('view engine', 'pug');
